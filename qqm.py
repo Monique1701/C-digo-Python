7afb6f5e47d41d255a56786ece280c341b7c8c6a
Deise <dcdeisecristiane03@gmail.com>
2019-11-22 14:09:30 -0300

    codigo sem espaços no nome

--- qqm.py
+++ qqm.py
@@ -12,9 +12,13 @@
 # Seu jogo não deve considerar a mesma pergunta duas vezes
 import random
 from time import sleep
+
 print("------*"*31)
 print ("Vem brincar! Vem Curtir! Vem testar seu conhecimentos!")
 print("------*"*31)
+
+
+
 
 print("Divirta-se na brincadeira do 'Quem quer ser um miliónario' ?")
 pontos = 0
@@ -22,21 +26,22 @@ lista = []
 pontos = 0
 nomeDojogador = ''
 
-listaIndicesPerguntas = [i for i in range(1,14)]
+listaIndicesPerguntas = [i for i in range(1,16)]
 #--------------------------------------------------------------------------------------------------------------------------------------------------------#
 #Cadastro de jogadores.
 def cadastroDeJogadores():
     global nomeDojogador
     while True:
         nomeDojogador = (input("Informe seu nome: "))
+        nomeDojogador = nomeDojogador.replace(' ','')
         if nomeDojogador == '':
             print('================================')
             print('  Informação inválida\nDigite algum nome para se cadastrar no jogo')
             print('================================')
             continue
         else:
-            print(f'Jogador {nomeDojogador} cadastrado!')
-            print('--------------Star--------------------', flush=True)
+            print('Jogador {} cadastrado!'.format(nomeDojogador))
+            print('--------------Play--------------------', flush=True)
             sleep(2)
             break
 #---------------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -85,22 +90,27 @@ def exibirRanking():
 #Função que abre o arquivo para a leitura e Varre o conteúdo do arquivo e Colocar em uma lista.
 def getPerguntas():
 
-    indice = random.choice(listaIndicesPerguntas)
-    listaIndicesPerguntas.remove(indice)
+    if len(listaIndicesPerguntas) == 0:
+        print('--------------------------------------- O JOGO ACABOU ---------------------------------------')
+    else:
+        indice = random.choice(listaIndicesPerguntas)
+        listaIndicesPerguntas.remove(indice)
 
-    arq = open('pergunta{}.txt'.format(indice),'r')
+        arq = open('pergunta{}.txt'.format(indice),'r')
 
-    for i in arq:
-        lista.append(i)
+        for i in arq:
+            lista.append(i)
 #
 def limparLista():
     lista.clear()
+
 #Nessa função contém o menu onde o jogador está interagindo 
 def menu():
     global pontos
     
     cadastroDeJogadores()
     resposta = (input("Deseja começar? [S/N]: "))
+    print('----'*35)
         
     if resposta.lower() == 'n':
         pass
@@ -124,21 +134,24 @@ def menu():
                 
                 if resp.lower() == respCorreta:
                     print("PROCESSSANDO.......")
-                    sleep(2)
+                    sleep(0)
                     print("Parabéns!")
-                    pontos = pontos + 667
+                    pontos = pontos + 100000
                     print("Sua pontuação é de {}".format(pontos), flush=True)
-                    sleep(3)
+                    print("---------------------------------------------*-------------------------------------------------")
+                    sleep(1)
                     break
                 elif resp not in alternativas:
+                    print("------------------------------------------------------------------------------------------------")
                     print('##################################################################')
-                    print('Resposta inválida. Digite uma opção correspondente as alternativas')
+                    print('Resposta inválida. Digite uma opção correspondente as alternativas') #Tratamento de erros
                     print('##################################################################')
                     continue
                 else:
-                    sleep(1)
+                    sleep(0)
                     print("Resposta incorreta!")
                     print("Infelizmente Não Foi Dessa Vez, Tente Novamente Mais Tarde. Não Desista!")
+                    print('-------------------------------------------------------------------------------------------------')
                     break
 
             limparLista()
